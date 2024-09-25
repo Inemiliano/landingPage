0a3e1e2bf7cb54c0877c58bafd37c111ba597de5
gilberto <emigil3010@gmail.com>
2024-09-25 01:09:08 -0600

    add:ultima vista en proceso de terminar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './home/principal/principal.component';
 import { MenuComponent } from './home/menu/menu.component';
+import { FinalComponent } from './home/final/final.component';
 
 const routes: Routes = [
   {path:'home',component:PrincipalComponent},
-  {path:'menu',component:MenuComponent}
+  {path:'menu',component:MenuComponent},
+  {path:'final',component:FinalComponent}
 ];
 
 @NgModule({
