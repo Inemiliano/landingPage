36eac065e59c1ace9f775346ed39271c131d7542
gilberto <emigil3010@gmail.com>
2024-09-24 20:41:20 -0600

    add:Enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './home/principal/principal.component';
+import { MenuComponent } from './home/menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:PrincipalComponent},
+  {path:'menu',component:MenuComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
